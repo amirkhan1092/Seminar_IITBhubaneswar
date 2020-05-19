16f9774a5fb7b952e974b2b1cea2115ff1b13246
amirkhan1092 <amirkhan1092@gmail.com>
2020-05-19 16:00:01 +0530

    capture image from webcame and hold figure add comment

--- opencv_basics.py
+++ opencv_basics.py
@@ -3,9 +3,12 @@ import cv2
 
 web = cv2.VideoCapture(0)
 
-ref, img = web.read()
-cv2.imshow('image', img)
+ref, img = web.read()  # take snapshot from web camera object
+cv2.imshow('image', img)  # figure title is image and img is the image in rgb form
 
 cv2.waitKey(False)  # hold the image figure until user press any key from user
 cv2.destroyAllWindows()
+
+
+
 
